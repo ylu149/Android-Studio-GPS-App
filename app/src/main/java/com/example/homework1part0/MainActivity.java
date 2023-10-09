ee6856daa2a119a80a129c6980a1d3f2af095259
Yang Lu <ylu149@bu.edu>
2023-10-09 12:30:24 -0400

    Set up android studio skeleton code and implemented get location methods.

---
+++ app/src/main/java/com/example/homework1part0/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.homework1part0;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+public class MainActivity extends AppCompatActivity {
+    private final int PERMISSION_ID = 238947;
+    private FusedLocationProviderClient mFusedLocationClient;
+    TextView locationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationView = findViewById(R.id.location);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+        Button button = findViewById(R.id.getLocation);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startLocationUpdates();
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                locationView.setText("Getting Location...");
+                LocationRequest locationRequest = LocationRequest.create();
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                locationRequest.setInterval(1000); // 1 seconds
+                locationRequest.setFastestInterval(500); // 0.5 seconds
+
+                mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.getMainLooper());
+            } else {
+                Toast.makeText(this, "Please enable location.", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+    //Pulled from online source: https://www.geeksforgeeks.org/how-to-get-user-location-in-android/
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+    //end of copied section
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            if (locationResult != null) {
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    locationView.setText("Longitude: " + location.getLongitude() + "\nLatitude: " + location.getLatitude());
+                }
+            }
+        }
+    };
+
+}
