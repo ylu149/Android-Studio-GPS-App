47bba9ea50e8235bf73e93fee44f151d6c194b48
darrowball13 <darrowry@bu.edu>
2023-10-09 16:26:42 -0400

    Updated logic for different speeds. Also tried to add a spinner to be used for choosing between the different speed options. Updated logic to change the color based on speed

--- app/src/main/java/com/example/homework1part0/MainActivity.java
+++ app/src/main/java/com/example/homework1part0/MainActivity.java
@@ -11,9 +11,12 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,17 +24,48 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     private final int PERMISSION_ID = 238947;
     private FusedLocationProviderClient mFusedLocationClient;
     TextView locationView, speedValue;
+
+    /*
+    Used following source to implement a spinner for selecting different speed
+    options: https://www.geeksforgeeks.org/spinner-in-android-using-java-with-example/#
+     */
+
+    String[] speedChoices = { "Miles per Hour",
+            "Kilometers per Hour", "Feet per Second"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         locationView = findViewById(R.id.location);
-
         speedValue = findViewById(R.id.speedValue);
+
+        /*
+        source for adding spinner: https://developer.android.com/develop/ui/views/components/spinner
+        In addition to source above
+         */
+
+        Spinner speedUnits = findViewById(R.id.speeds_spinner);
+        speedUnits.setOnItemSelectedListener(this);
+
+        // Create the array containing the list of choices
+        ArrayAdapter ad
+                = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                speedChoices);
+
+        // set simple layout resource file for each item of spinner
+        ad.setDropDownViewResource(
+                android.R.layout
+                        .simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter data on the Spinner which binds data to spinner
+        speedUnits.setAdapter(ad);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -43,28 +77,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-
-
-    @SuppressLint("MissingPermission")
-    private void startLocationUpdates() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                locationView.setText("Getting Location...");
-                LocationRequest locationRequest = LocationRequest.create();
-                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                locationRequest.setInterval(1000); // 1 seconds
-                locationRequest.setFastestInterval(500); // 0.5 seconds
-                mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.getMainLooper());
-            } else {
-                Toast.makeText(this, "Please enable location.", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            requestPermissions();
-        }
     }
 
     //Pulled from online source: https://www.geeksforgeeks.org/how-to-get-user-location-in-android/
@@ -86,6 +98,27 @@ public class MainActivity extends AppCompatActivity {
     }
     //end of copied section
 
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                locationView.setText("Getting Location...");
+                LocationRequest locationRequest = LocationRequest.create();
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                locationRequest.setInterval(1000); // 1 seconds
+                locationRequest.setFastestInterval(500); // 0.5 seconds
+                mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.getMainLooper());
+            } else {
+                Toast.makeText(this, "Please enable location.", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+
     private LocationCallback mLocationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(LocationResult locationResult) {
@@ -94,11 +127,55 @@ public class MainActivity extends AppCompatActivity {
                 Location location = locationResult.getLastLocation();
                 if (location != null) {
                     locationView.setText("Longitude: " + location.getLongitude() + "\nLatitude: " + location.getLatitude());
-                    speedValue.setText(String.valueOf(location.getSpeed()));
+
+                    float speedMeters = location.getSpeed();
+
+                    speedColors(speedMeters);
+                    //speedUnits(speedMeters);
+
                 }
             }
         }
     };
+
+
+
+    //Change the speedValue fields color based on the speed calculated
+    private void speedColors(float speedMeters) {
+        if(speedMeters <= 5) {
+            speedValue.setTextColor(Integer.parseInt("green"));
+        } else if (speedMeters >5 && speedMeters <= 10) {
+            speedValue.setTextColor(Integer.parseInt("yellow"));
+        } else {
+            speedValue.setTextColor(Integer.parseInt("red"));
+        }
+    }
+
+    //Change the Units based on the Spinner item selected
+
+    /*
+    Commented out for time being while troubleshooting other things
+
+    private void speedUnits(float speedMeters) {
+
+        double speedCalc;
+
+        if(spinner_choice = "Miles per Hour") {
+            speedCalc = speedMeters * 2.23694;
+            speedValue.setText(String.valueOf(speedCalc));
+        } else if (spinner_choice = "Kilometers per Hour") {
+            speedCalc = speedMeters * 3.60000;
+            speedValue.setText(String.valueOf(speedCalc));
+        } else if (spinner_choice = "Feet per Second") {
+            speedCalc = speedMeters * 3.28084;
+            speedValue.setText(String.valueOf(speedCalc));
+        } else {
+            speedValue.setText(String.valueOf(speedMeters));
+        }
+
+    }
+
+     */
 
     private void stopLocationUpdates() {
         if (mLocationCallback != null) {
@@ -106,6 +183,22 @@ public class MainActivity extends AppCompatActivity {
             mLocationCallback = null;
         }
     }
+
+    /*
+    Unfilled onItemSelected and onNothingSelected Methods for spinners
+     */
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    /*
     @Override
     protected void onPause() {
         super.onPause();
@@ -117,5 +210,5 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         startLocationUpdates();
     }
-
+    */
 }
